c8a6c020ed47b01b80b88dddf85443663a04d968
Lisiane Ambiel <lisianem@br.ibm.com>
2017-01-12 19:13:56 +0000

    Create new job to trigger nightly build using extra arguments
    Nightly builds run build_host_os using Mock arguments to enable tests (check phase).
    It also removes a line with echo command from trigger_weekly pre_build_script.

---
+++ jenkins_jobs/trigger_nightly_host_os_build/post_build_script.sh
@@ -0,0 +1,15 @@
+NIGHTLY_DIR_NAME=$(cat NIGHTLY_DIR_NAME)
+BUILD_DIR_PATH="../to_build/${TRIGGERED_BUILD_NUMBER_build_host_os}"
+
+# update nightly build dir and latest nightly build
+ln -s "$BUILD_DIR_PATH" "$NIGHTLY_DIR_NAME"
+ln -s "$NIGHTLY_DIR_NAME" latest
+
+rsync -e "ssh -i $HOME/.ssh/${UPLOAD_SERVER_USER_NAME}_id_rsa" \
+      --verbose --compress --links --times --chmod=a+rwx,g+rwx,o- \
+      "$NIGHTLY_DIR_NAME" \
+      "${UPLOAD_SERVER_USER_NAME}@${UPLOAD_SERVER_HOST_NAME}:${UPLOAD_SERVER_NIGHTLY_DIR}/"
+rsync -e "ssh -i $HOME/.ssh/${UPLOAD_SERVER_USER_NAME}_id_rsa" \
+      --verbose --compress --links --times --chmod=a+rwx,g+rwx,o- \
+      "latest" \
+      "${UPLOAD_SERVER_USER_NAME}@${UPLOAD_SERVER_HOST_NAME}:${UPLOAD_SERVER_NIGHTLY_DIR}/"

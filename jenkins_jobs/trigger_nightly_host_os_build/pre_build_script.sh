e092cf0cf95e623400e2504bed488cb9020969b3
Lisiane Ambiel <lisianem@br.ibm.com>
2017-01-26 19:31:37 +0000

    Execute upgrade-versions on nightly builds

--- jenkins_jobs/trigger_nightly_host_os_build/pre_build_script.sh
+++ jenkins_jobs/trigger_nightly_host_os_build/pre_build_script.sh
@@ -1,2 +1,15 @@
 RELEASE_DATE=$(date +%Y-%m-%d)
+COMMIT_BRANCH="nightly-${RELEASE_DATE}"
+
+# Upgrade versions
+# sudo yum install rpmdevtools
+python host_os.py \
+   --verbose \
+   upgrade-versions \
+       --committer-name "$GITHUB_BOT_NAME" \
+       --committer-email "$GITHUB_BOT_EMAIL" \
+       --push-repo-url "ssh://git@github.com/${GITHUB_BOT_USER_NAME}/versions.git" \
+       --push-repo-branch "$COMMIT_BRANCH"
+
+echo "VERSIONS_REPO_COMMIT=$COMMIT_BRANCH" > BUILD_PARAMETERS
 echo "$RELEASE_DATE" > NIGHTLY_DIR_NAME

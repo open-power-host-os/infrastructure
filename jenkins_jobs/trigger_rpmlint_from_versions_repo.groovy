a9ee48042aeacf5fbaec3c9f1f348f59c031ef35
Alan Evangelista <alanoe@linux.vnet.ibm.com>
2017-02-06 18:42:31 -0200

    Add Jenkins job to run rpmlint in versions git repository

---
+++ jenkins_jobs/trigger_rpmlint_from_versions_repo.groovy
@@ -0,0 +1,50 @@
+job('trigger_rpmlint_from_versions_repo') {
+  label('validation_slave_label')
+  concurrentBuild()
+  parameters {
+    stringParam('BUILDS_REPO_URL',
+		"https://github.com/${GITHUB_ORGANIZATION_NAME}/builds.git",
+		'URL of the builds repository.')
+    stringParam('BUILDS_REPO_COMMIT', 'master',
+		'Commit ID to checkout from the builds repository.')
+    stringParam('VERSIONS_REPO_URL',
+		"https://github.com/${GITHUB_ORGANIZATION_NAME}/versions.git",
+		'URL of the versions repository.')
+    stringParam('sha1', 'master',
+		'Commit ID to checkout from the versions repository.')
+  }
+  properties {
+    githubProjectUrl("https://github.com/${GITHUB_ORGANIZATION_NAME}/versions/")
+  }
+  scm {
+    git {
+      remote {
+	    url('$BUILDS_REPO_URL')
+      }
+      branch('$BUILDS_REPO_COMMIT')
+    }
+  }
+  triggers {
+    githubPullRequest {
+      userWhitelist("${GHPRB_ADMIN_USER}")
+      orgWhitelist("${GHPRB_ADMIN_ORGANIZATION}")
+      allowMembersOfWhitelistedOrgsAsAdmin()
+      cron('H/5 * * * *')
+      triggerPhrase('.*start\\W+(tests|rpmlint).*')
+      extensions {
+	commitStatus {
+	  context('RPMlint')
+	}
+      }
+    }
+  }
+  steps {
+    shell(
+      readFileFromWorkspace(
+	    'jenkins_jobs/trigger_rpmlint_from_versions_repo/script.sh'))
+  }
+  wrappers {
+    timestamps()
+    preBuildCleanup()
+  }
+}

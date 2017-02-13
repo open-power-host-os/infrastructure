open-power-host-os/infrastructure

commit 9cf81de464cce722d2a7a5a547bd347c95218b67

Execute upgrade-versions on nightly builds
Since the commit will not be merged into the main repository, the local
repository path is passed to the build_host_os job.

jenkins_jobs/trigger_nightly_host_os_build.groovy
@@ -10,6 +10,10 @@ job('trigger_nightly_host_os_build') {
     stringParam('BUILDS_REPOSITORY_BRANCH',
 		"master",
 		'Branch of the builds repository to clone and pass to the build jobs.')
+    stringParam('GITHUB_BOT_NAME', "${GITHUB_BOT_NAME}",
+		'Name of the GitHub user to create commits automatically.')
+    stringParam('GITHUB_BOT_EMAIL', "${GITHUB_BOT_EMAIL}",
+		'Email of the GitHub user to create commits automatically')
     stringParam('UPLOAD_SERVER_HOST_NAME', "${UPLOAD_SERVER_HOST_NAME}",
 		'Host name of the target server to upload build results.')
     stringParam('UPLOAD_SERVER_USER_NAME',
@@ -46,9 +50,9 @@ job('trigger_nightly_host_os_build') {
 	     'https://github.com/${GITHUB_ORGANIZATION_NAME}/builds.git',
 	     BUILDS_REPO_COMMIT: '$BUILDS_REPOSITORY_BRANCH',
 	     VERSIONS_REPO_URL:
-	     'https://github.com/${GITHUB_ORGANIZATION_NAME}/versions.git',
-             VERSIONS_REPO_COMMIT: 'master',
+	     'file://${WORKSPACE}/versions',
              EXTRA_PARAMETERS: '--mock-args "--enable-plugin=tmpfs --plugin-option=tmpfs:keep_mounted=True --plugin-option=tmpfs:max_fs_size=32g --plugin-option=tmpfs:required_ram_mb=39800 --with tests"'])
+          propertiesFile("BUILD_PARAMETERS", true)
 	}
       }
     }

jenkins_jobs/trigger_nightly_host_os_build/pre_build_script.sh
@@ -1,2 +1,13 @@
 RELEASE_DATE=$(date +%Y-%m-%d)
+
+# Upgrade versions
+# sudo yum install rpmdevtools
+python host_os.py \
+   --verbose \
+   upgrade-versions \
+       --no-push-updates \
+       --updater-name "$GITHUB_BOT_NAME" \
+       --updater-email "$GITHUB_BOT_EMAIL"
+
+echo "VERSIONS_REPO_COMMIT=HEAD" > BUILD_PARAMETERS
 echo "$RELEASE_DATE" > NIGHTLY_DIR_NAME

commit ccd7d1d0bc61f01272ef3f91f53400cb99ede1cc

Fix nightly builds
Push the commit created by upgrade-versions command in nightly job to
the bot user's repository in GitHub.
The nightly builds job was passing a local URL to the build job, which
could not be accessed if they were executed in different slaves.

Regression introduced in commit 9cf81de464cce722d2a7a5a547bd347c95218b67

jenkins_jobs/trigger_nightly_host_os_build.groovy
@@ -12,6 +12,8 @@ job('trigger_nightly_host_os_build') {
 		'Branch of the builds repository to clone and pass to the build jobs.')
     stringParam('GITHUB_BOT_NAME', "${GITHUB_BOT_NAME}",
 		'Name of the GitHub user to create commits automatically.')
+    stringParam('GITHUB_BOT_USER_NAME', "${GITHUB_BOT_USER_NAME}",
+		'User name of the GitHub user to create commits automatically.')
     stringParam('GITHUB_BOT_EMAIL', "${GITHUB_BOT_EMAIL}",
 		'Email of the GitHub user to create commits automatically')
     stringParam('UPLOAD_SERVER_HOST_NAME', "${UPLOAD_SERVER_HOST_NAME}",
@@ -50,7 +52,7 @@ job('trigger_nightly_host_os_build') {
 	     'https://github.com/${GITHUB_ORGANIZATION_NAME}/builds.git',
 	     BUILDS_REPO_COMMIT: '$BUILDS_REPOSITORY_BRANCH',
 	     VERSIONS_REPO_URL:
-	     'file://${WORKSPACE}/versions',
+	     "https://github.com/${GITHUB_BOT_USER_NAME}/versions.git",
              EXTRA_PARAMETERS: '--mock-args "--enable-plugin=tmpfs --plugin-option=tmpfs:keep_mounted=True --plugin-option=tmpfs:max_fs_size=32g --plugin-option=tmpfs:required_ram_mb=39800 --with tests"'])
           propertiesFile("BUILD_PARAMETERS", true)
 	}

jenkins_jobs/trigger_nightly_host_os_build/pre_build_script.sh
@@ -1,13 +1,16 @@
+VERSIONS_REPO_URL="https://github.com/${GITHUB_ORGANIZATION_NAME}/versions.git"
 RELEASE_DATE=$(date +%Y-%m-%d)
+COMMIT_BRANCH="nightly-${RELEASE_DATE}"
 
 # Upgrade versions
 # sudo yum install rpmdevtools
 python host_os.py \
    --verbose \
    upgrade-versions \
-       --no-push-updates \
        --updater-name "$GITHUB_BOT_NAME" \
-       --updater-email "$GITHUB_BOT_EMAIL"
+       --updater-email "$GITHUB_BOT_EMAIL" \
+       --push-repo-url "ssh://git@github.com/${GITHUB_BOT_USER_NAME}/versions.git" \
+       --push-repo-branch "$COMMIT_BRANCH"
 
-echo "VERSIONS_REPO_COMMIT=HEAD" > BUILD_PARAMETERS
+echo "VERSIONS_REPO_COMMIT=$COMMIT_BRANCH" > BUILD_PARAMETERS
 echo "$RELEASE_DATE" > NIGHTLY_DIR_NAME
c7582bd06f11c019d0d6e08903761f9141682079
Olav Philipp Henschel <olavph@linux.vnet.ibm.com>
2017-02-20 16:00:43 -0300

    Turn rsync_uploads from aliases to functions
    This simplifies the upload scripts, making them more legible.

--- jenkins_jobs/upload_iso/script.sh
+++ jenkins_jobs/upload_iso/script.sh
@@ -3,13 +3,15 @@ BUILD_DIR_PATH="${UPLOAD_SERVER_BUILDS_DIR}/${BUILD_TIMESTAMP}"
 BUILD_DIR_RSYNC_URL="${UPLOAD_SERVER_USER_NAME}@${UPLOAD_SERVER_HOST_NAME}:${BUILD_DIR_PATH}"
 ISO_DIR_RSYNC_URL="${BUILD_DIR_RSYNC_URL}/iso"
 
-alias rsync_upload="rsync -e \
-    'ssh -i ${HOME}/.ssh/${UPLOAD_SERVER_USER_NAME}_id_rsa' \
-    --verbose --compress --stats --times --chmod=a+rwx,g+wx,o-"
+rsync_upload() {
+    rsync -e 'ssh -i ${HOME}/.ssh/${UPLOAD_SERVER_USER_NAME}_id_rsa' \
+              --verbose --compress --stats --times --chmod=a+rwx,g+wx,o- \
+              $@ $ISO_DIR_RSYNC_URL
+}
 
 # Create remote iso directory
 mkdir iso
-rsync_upload --recursive iso $BUILD_DIR_RSYNC_URL
+rsync_upload --recursive iso
 
-rsync_upload *.iso $ISO_DIR_RSYNC_URL
-rsync_upload *-CHECKSUM $ISO_DIR_RSYNC_URL
+rsync_upload *.iso
+rsync_upload *-CHECKSUM

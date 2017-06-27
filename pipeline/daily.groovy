c3afc010f6f6b72f13455cadf13b16de936dd93a
Fabiano Rosas <farosas@linux.vnet.ibm.com>
2017-06-26 20:02:07 -0500

    Add Slack notifications to daily builds
    Since these builds are completely automated and run every single day,
    the effects of their failure might go unnoticed for too long. Slack
    notifications will help shorten our reaction time.

--- pipeline/daily.groovy
+++ pipeline/daily.groovy
@@ -4,34 +4,41 @@ pipelineStages = load 'infrastructure/pipeline/daily/stages.groovy'
 
 def execute() {
   timestamps {
-    stage('Initialize') {
-      pipelineStages.initialize()
-    }
-
-    node('builds_slave_label') {
-      lock(resource: "update-versions_workspace_$env.NODE_NAME") {
-        stage('Update packages versions') {
-          pipelineStages.updateVersions()
-        }
-
-        stage('Build packages') {
-          pipelineStages.buildPackages()
-        }
-
-        if (currentBuild.result != 'FAILURE') {
-          stage('Build ISO') {
-            pipelineStages.buildIso()
-          }
-        }
-
-        stage('Upload build artifacts') {
-          pipelineStages.uploadBuildArtifacts()
-        }
-
-        stage('Create symlinks') {
-          pipelineStages.createSymlinks()
-        }
+    try {
+      stage('Initialize') {
+	pipelineStages.initialize()
       }
+
+      node('builds_slave_label') {
+	lock(resource: "update-versions_workspace_$env.NODE_NAME") {
+	  stage('Update packages versions') {
+	    pipelineStages.updateVersions()
+	  }
+
+	  stage('Build packages') {
+	    pipelineStages.buildPackages()
+	  }
+
+	  if (currentBuild.result != 'FAILURE') {
+	    stage('Build ISO') {
+	      pipelineStages.buildIso()
+	    }
+	  }
+
+	  stage('Upload build artifacts') {
+	    pipelineStages.uploadBuildArtifacts()
+	  }
+
+	  stage('Create symlinks') {
+	    pipelineStages.createSymlinks()
+	  }
+	}
+      }
+    } catch (Exception exception) {
+      if (pipelineStages.shouldNotifyOnFailure()) {
+	pipelineStages.notifyFailure()
+      }
+      throw exception
     }
   }
 }

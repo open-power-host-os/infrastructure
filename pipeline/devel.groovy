1800665948d9c4d673e02a84bc331ef64f52d937
Olav Philipp Henschel <olavph@linux.vnet.ibm.com>
2017-08-15 14:18:42 -0300

    Fix indentation in devel.groovy

--- pipeline/devel.groovy
+++ pipeline/devel.groovy
@@ -11,37 +11,37 @@ def execute() {
   timestamps {
     try {
       stage('Initialize') {
-	pipelineStages.initialize()
+        pipelineStages.initialize()
       }
 
       node('builds_slave_label') {
-	lock(resource: "update-versions_workspace_$env.NODE_NAME") {
-	  stage('Update packages versions') {
-	    pipelineStages.updateVersions()
-	  }
+        lock(resource: "update-versions_workspace_$env.NODE_NAME") {
+          stage('Update packages versions') {
+            pipelineStages.updateVersions()
+          }
 
-	  stage('Build packages') {
-	    pipelineStages.buildPackages()
-	  }
+          stage('Build packages') {
+            pipelineStages.buildPackages()
+          }
 
-	  if (currentBuild.result != 'FAILURE') {
-	    stage('Build ISO') {
-	      pipelineStages.buildIso()
-	    }
-	  }
+          if (currentBuild.result != 'FAILURE') {
+            stage('Build ISO') {
+              pipelineStages.buildIso()
+            }
+          }
 
-	  stage('Upload build artifacts') {
-	    pipelineStages.uploadBuildArtifacts()
-	  }
+          stage('Upload build artifacts') {
+            pipelineStages.uploadBuildArtifacts()
+          }
 
-	  stage('Create symlinks') {
-	    pipelineStages.createSymlinks()
-	  }
-	}
+          stage('Create symlinks') {
+            pipelineStages.createSymlinks()
+          }
+        }
       }
     } catch (Exception exception) {
       if (pipelineStages.shouldNotifyOnFailure()) {
-	pipelineStages.notifyFailure()
+        pipelineStages.notifyFailure()
       }
       throw exception
     }

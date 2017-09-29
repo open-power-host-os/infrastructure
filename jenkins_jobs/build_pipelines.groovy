fd54c47c2cdb77a84db4c5e6da6b1e92ddbedb7e
Olav Philipp Henschel <olavph@linux.vnet.ibm.com>
2017-09-29 10:27:26 -0300

    Parameterize GitHub domain for PRs automatic checkout
    The GitHub plugin was always monitoring the public GitHub domain,
    regardless of the variable GITHUB_DOMAIN set by Ansible. This commit
    fixes that inconsistency.

--- jenkins_jobs/build_pipelines.groovy
+++ jenkins_jobs/build_pipelines.groovy
@@ -2,6 +2,7 @@ def createPipeline(String repositoryName) {
   multibranchPipelineJob(repositoryName) {
     branchSources {
       github {
+        apiUri("https://$GITHUB_DOMAIN/api/v3")
         scanCredentialsId('github-user-pass-credentials')
         repoOwner(GITHUB_ORGANIZATION_NAME)
         repository(repositoryName)

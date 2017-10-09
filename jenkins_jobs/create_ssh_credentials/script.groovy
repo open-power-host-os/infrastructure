271362c31c1090396cb596bd2ae863d63ce5d49f
Olav Philipp Henschel <olavph@linux.vnet.ibm.com>
2017-10-09 15:19:50 -0300

    Make credentials jobs rebuildable
    The jobs would fail if credentials with the specified ID already
    existed. They now remove the existing matching credentials before
    creating new ones.

--- jenkins_jobs/create_ssh_credentials/script.groovy
+++ jenkins_jobs/create_ssh_credentials/script.groovy
@@ -5,15 +5,29 @@ import com.cloudbees.plugins.credentials.domains.*
 import com.cloudbees.jenkins.plugins.sshcredentials.impl.*
 import hudson.plugins.sshslaves.*;
 
+String credentialsId = build.buildVariableResolver.resolve("CREDENTIALS_ID")
+String userName = build.buildVariableResolver.resolve("USER_NAME")
+String sshPrivateKeyPass = build.buildVariableResolver.resolve("SSH_PRIVATE_KEY_PATH")
+
 global_domain = Domain.global()
 credentials_store = Jenkins.instance.getExtensionList(
     'com.cloudbees.plugins.credentials.SystemCredentialsProvider'
     )[0].getStore()
-credentials = new BasicSSHUserPrivateKey(
+
+oldCredentials = credentials_store.getCredentials(global_domain).findResult {
+  it.id == credentialsId ? it : null
+}
+if (oldCredentials != null) {
+  println("Removing old credentials with ID '$credentialsId'")
+  credentials_store.removeCredentials(global_domain, oldCredentials)
+}
+
+newCredentials = new BasicSSHUserPrivateKey(
     CredentialsScope.GLOBAL,
-    build.buildVariableResolver.resolve("CREDENTIALS_ID"),
-    build.buildVariableResolver.resolve("USER_NAME"),
+    credentialsId,
+    userName,
     new BasicSSHUserPrivateKey.FileOnMasterPrivateKeySource(
-        build.buildVariableResolver.resolve("SSH_PRIVATE_KEY_PATH")), "", ""
+        sshPrivateKeyPass), "", ""
     )
-credentials_store.addCredentials(global_domain, credentials)
+println("Adding new credentials with ID '$credentialsId' and user name '$userName'")
+credentials_store.addCredentials(global_domain, newCredentials)
--- jenkins_jobs/create_user_pass_credentials/script.groovy
+++ jenkins_jobs/create_user_pass_credentials/script.groovy
@@ -4,14 +4,28 @@ import com.cloudbees.plugins.credentials.common.*
 import com.cloudbees.plugins.credentials.domains.*
 import com.cloudbees.plugins.credentials.impl.*;
 
+String credentialsId = build.buildVariableResolver.resolve("CREDENTIALS_ID")
+String userName = build.buildVariableResolver.resolve("USER_NAME")
+String password = build.buildVariableResolver.resolve("PASSWORD")
+
 global_domain = Domain.global()
 credentials_store = Jenkins.instance.getExtensionList(
     'com.cloudbees.plugins.credentials.SystemCredentialsProvider'
     )[0].getStore()
-credentials = new UsernamePasswordCredentialsImpl(
+
+oldCredentials = credentials_store.getCredentials(global_domain).findResult {
+  it.id == credentialsId ? it : null
+}
+if (oldCredentials != null) {
+  println("Removing old credentials with ID '$credentialsId'")
+  credentials_store.removeCredentials(global_domain, oldCredentials)
+}
+
+newCredentials = new UsernamePasswordCredentialsImpl(
     CredentialsScope.GLOBAL,
-    build.buildVariableResolver.resolve("CREDENTIALS_ID"),
+    credentialsId,
     "",
-    build.buildVariableResolver.resolve("USER_NAME"),
-    build.buildVariableResolver.resolve("PASSWORD"))
-credentials_store.addCredentials(global_domain, credentials)
+    userName,
+    password)
+println("Adding new credentials with ID '$credentialsId' and user name '$userName'")
+credentials_store.addCredentials(global_domain, newCredentials)

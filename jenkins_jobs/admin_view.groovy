2275ca0e0a6490eabc2d01d3429d987cca070733
Olav Philipp Henschel <olavph@linux.vnet.ibm.com>
2017-10-10 13:17:25 -0300

    Create credentials job via Ansible
    The seed job might need credentials to access GitHub if an enterprise
    instance is used. Executing the seed job before creating the credentials
    was also problematic because the pipelines would start monitoring the
    repositories, but not have write access to update pull requests statuses,
    for example.
    
    By creating the credentials before anything else is executed on Jenkins,
    we fix those problems and catch authentication issues earlier in the
    setup.

--- jenkins_jobs/admin_view.groovy
+++ jenkins_jobs/admin_view.groovy
@@ -1,9 +1,8 @@
 listView('Admin') {
     description('Administrative jobs')
     jobs {
+        name('create_credentials')
         name('create_node')
-        name('create_ssh_credentials')
-        name('create_user_pass_credentials')
         name('seed_job')
     }
     columns {
